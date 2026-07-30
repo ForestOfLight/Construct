fdea01abc7c689aa13bc348d3895f5d19d556fcb
ForestOfLight <briancassriel@gmail.com>
2026-07-29 18:11:53 -0700

    feat: add Java block model parent-chain resolver

---
+++ filters/fetch_block_models/model_resolver.py
@@ -0,0 +1,60 @@
+"""Resolves a Java block model's parent chain into concrete elements with
+textures fully resolved to literal names (no #variable indirection left)."""
+
+MODEL_PATH_TMPL = "assets/minecraft/models/{path}.json"
+
+
+def load_model(mcmeta, model_id):
+    path = model_id.split(":")[-1]
+    return mcmeta.read_json(MODEL_PATH_TMPL.format(path=path))
+
+
+def resolve_model(mcmeta, model_id):
+    """Returns a list of elements: [{'from': [x,y,z], 'to': [x,y,z],
+    'faces': {face_name: {'uv','texture','rotation','cullface','tintindex'}}}]"""
+    chain = []
+    current_id = model_id
+    seen = set()
+    while current_id and current_id not in seen:
+        seen.add(current_id)
+        model = load_model(mcmeta, current_id)
+        chain.append(model)
+        current_id = model.get("parent")
+    chain.reverse()  # root first, most specific last
+
+    textures = {}
+    elements = None
+    for model in chain:
+        textures.update(model.get("textures", {}))
+        if "elements" in model:
+            elements = model["elements"]
+
+    resolved_elements = []
+    for element in elements or []:
+        resolved_faces = {}
+        for face_name, face in element.get("faces", {}).items():
+            resolved_faces[face_name] = {
+                "uv": face.get("uv", [0, 0, 16, 16]),
+                "texture": _resolve_texture_ref(face["texture"], textures),
+                "rotation": face.get("rotation", 0),
+                "cullface": face.get("cullface"),
+                "tintindex": face.get("tintindex", -1),
+            }
+        resolved_elements.append({
+            "from": element["from"],
+            "to": element["to"],
+            "faces": resolved_faces,
+        })
+    return resolved_elements
+
+
+def _resolve_texture_ref(ref, textures, depth=0):
+    """Follows '#var' indirection to a literal 'block/xxx' texture name."""
+    if depth > 10:
+        raise ValueError(f"texture reference cycle at {ref!r}")
+    if isinstance(ref, str) and ref.startswith("#"):
+        var = ref[1:]
+        if var not in textures:
+            raise KeyError(f"unresolved texture variable '#{var}'")
+        return _resolve_texture_ref(textures[var], textures, depth + 1)
+    return ref
---
+++ filters/fetch_block_models/test_model_resolver.py
@@ -0,0 +1,90 @@
+import unittest
+
+from model_resolver import resolve_model
+
+
+class FakeMcmeta:
+    def __init__(self, models):
+        self._models = models  # {'block/x': {...}}
+
+    def read_json(self, path):
+        # path looks like 'assets/minecraft/models/block/x.json'
+        model_id = path[len("assets/minecraft/models/"):-len(".json")]
+        return self._models[model_id]
+
+
+class ResolveModelTest(unittest.TestCase):
+    def test_inherits_elements_from_parent_when_child_has_none(self):
+        mcmeta = FakeMcmeta({
+            "block/cube_all": {
+                "textures": {"particle": "#all"},
+                "elements": [{
+                    "from": [0, 0, 0], "to": [16, 16, 16],
+                    "faces": {
+                        "up": {"texture": "#all", "uv": [0, 0, 16, 16]},
+                    },
+                }],
+            },
+            "block/stone": {
+                "parent": "block/cube_all",
+                "textures": {"all": "block/stone"},
+            },
+        })
+        elements = resolve_model(mcmeta, "block/stone")
+        self.assertEqual(len(elements), 1)
+        self.assertEqual(elements[0]["from"], [0, 0, 0])
+        self.assertEqual(elements[0]["to"], [16, 16, 16])
+        self.assertEqual(elements[0]["faces"]["up"]["texture"], "block/stone")
+
+    def test_child_elements_override_parent_elements(self):
+        mcmeta = FakeMcmeta({
+            "block/parent": {
+                "textures": {},
+                "elements": [{"from": [0, 0, 0], "to": [16, 16, 16], "faces": {}}],
+            },
+            "block/child": {
+                "parent": "block/parent",
+                "textures": {"all": "block/child_tex"},
+                "elements": [{
+                    "from": [4, 0, 4], "to": [12, 8, 12],
+                    "faces": {"up": {"texture": "#all", "uv": [0, 0, 8, 8]}},
+                }],
+            },
+        })
+        elements = resolve_model(mcmeta, "block/child")
+        self.assertEqual(len(elements), 1)
+        self.assertEqual(elements[0]["from"], [4, 0, 4])
+        self.assertEqual(elements[0]["faces"]["up"]["texture"], "block/child_tex")
+
+    def test_face_defaults_for_missing_optional_fields(self):
+        mcmeta = FakeMcmeta({
+            "block/x": {
+                "textures": {"all": "block/x"},
+                "elements": [{
+                    "from": [0, 0, 0], "to": [16, 16, 16],
+                    "faces": {"north": {"texture": "#all"}},
+                }],
+            },
+        })
+        face = resolve_model(mcmeta, "block/x")[0]["faces"]["north"]
+        self.assertEqual(face["uv"], [0, 0, 16, 16])
+        self.assertEqual(face["rotation"], 0)
+        self.assertIsNone(face["cullface"])
+        self.assertEqual(face["tintindex"], -1)
+
+    def test_raises_on_unresolved_texture_variable(self):
+        mcmeta = FakeMcmeta({
+            "block/x": {
+                "textures": {},
+                "elements": [{
+                    "from": [0, 0, 0], "to": [16, 16, 16],
+                    "faces": {"up": {"texture": "#missing"}},
+                }],
+            },
+        })
+        with self.assertRaises(KeyError):
+            resolve_model(mcmeta, "block/x")
+
+
+if __name__ == "__main__":
+    unittest.main()
