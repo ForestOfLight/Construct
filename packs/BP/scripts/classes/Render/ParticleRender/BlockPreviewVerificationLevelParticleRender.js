a5e64338a6b3f4278b95c63b814d9e577c720edc
ForestOfLight <briancassriel@gmail.com>
2026-07-29 19:21:34 -0700

    feat: rewrite block preview render for per-face textured particles

--- packs/BP/scripts/classes/Render/ParticleRender/BlockPreviewVerificationLevelParticleRender.js
+++ packs/BP/scripts/classes/Render/ParticleRender/BlockPreviewVerificationLevelParticleRender.js
@@ -1,59 +1,67 @@
 import { MolangVariableMap } from "@minecraft/server";
 import { BlockVerificationLevel } from "../../Enums/BlockVerificationLevel";
 import { Vector } from "../../../lib/Vector";
+import { BlockModelLookup } from "../BlockModelLookup";
 
 export class BlockPreviewVerificationLevelParticleRender {
     lifetimeSeconds = 0;
 
-    constructor(dimensionLocation, verificationLevel, lifetimeSeconds = 5) {
+    constructor(dimensionLocation, targetPermutation, verificationLevel, lifetimeSeconds = 5) {
         this.dimension = dimensionLocation.dimension;
         this.location = Vector.from(dimensionLocation.location);
+        this.targetPermutation = targetPermutation;
         this.verificationLevel = verificationLevel;
         this.lifetimeSeconds = lifetimeSeconds;
         this.#renderBlock();
     }
 
     #renderBlock() {
-        const sizeScalar = this.#verificationLevelToSizeScalar();
-        const particle = this.#getBlockParticle();
-        const color = this.#getRGBAMolang();
-        if (!color)
+        const rgb = this.#verificationLevelToRGB();
+        if (!rgb || !this.targetPermutation)
             return;
-        color.setFloat("lifetime", this.lifetimeSeconds);
-        // color.setFloat("width", 0.5*sizeScalar);
-        // color.setFloat("height", 0.5*sizeScalar);
-        color.setFloat("size", 0.5*sizeScalar);
+        const material = this.#verificationLevelToMaterial();
+        for (const face of BlockModelLookup.getFaces(this.targetPermutation)) {
+            this.#renderFace(face, rgb, material);
+        }
+    }
+
+    #renderFace(face, rgb, material) {
+        const from = new Vector(face.from[0] / 16, face.from[1] / 16, face.from[2] / 16);
+        const to = new Vector(face.to[0] / 16, face.to[1] / 16, face.to[2] / 16);
+        const center = from.add(to).multiply(0.5);
+        const size = { x: Math.abs(to.x - from.x), y: Math.abs(to.y - from.y), z: Math.abs(to.z - from.z) };
+        const [width, height] = this.#faceDimensions(face.axis, size);
+        if (width === 0 || height === 0)
+            return;
+
+        const molang = new MolangVariableMap();
+        molang.setFloat("lifetime", this.lifetimeSeconds);
+        molang.setFloat("width", width);
+        molang.setFloat("height", height);
+        molang.setFloat("u", face.uv.x);
+        molang.setFloat("v", face.uv.y);
+        molang.setFloat("uv_w", face.uv.w);
+        molang.setFloat("uv_h", face.uv.h);
+        molang.setColorRGBA("face_color", rgb);
+
+        const particleType = `construct:block_face_${face.axis}_${material}`;
         try {
-            this.dimension.spawnParticle(particle.particleType, particle.location, color);
+            this.dimension.spawnParticle(particleType, this.location.add(center), molang);
         } catch {
             /* pass */
         }
     }
 
-    #getBlockParticle() {
-        const center = new Vector(0.5, 0.5, 0.5);
-        return { particleType: this.#verificationLevelToParticleType(), location: this.location.add(center) };
+    #faceDimensions(axis, size) {
+        if (axis === "xz")
+            return [size.x, size.z];
+        if (axis === "xy")
+            return [size.x, size.y];
+        return [size.y, size.z]; // yz
     }
 
-    #getRGBAMolang() {
-        const rgb = this.#verificationLevelToRGB();
-        if (!rgb) return;
-        const molang = new MolangVariableMap();
-        molang.setColorRGBA("face_color", rgb);
-        return molang;
-    }
-
-    #verificationLevelToParticleType() {
-        switch (this.verificationLevel) {
-            case BlockVerificationLevel.NoMatch:
-                return "construct:blockoverlay_controller_blend";
-            case BlockVerificationLevel.TypeMatch:
-                return "construct:blockoverlay_controller_blend";
-            case BlockVerificationLevel.Missing:
-                return "construct:blockoverlay_controller";
-            default:
-                return void 0;
-        }
+    #verificationLevelToMaterial() {
+        return this.verificationLevel === BlockVerificationLevel.Missing ? "opaque" : "blend";
     }
 
     #verificationLevelToRGB() {
@@ -66,19 +74,6 @@ export class BlockPreviewVerificationLevelParticleRender {
                 return { red: 0.5, green: 0.5, blue: 1, alpha: 1 };
             default:
                 return void 0;
-        }
-    }
-
-    #verificationLevelToSizeScalar() {
-        switch (this.verificationLevel) {
-            case BlockVerificationLevel.NoMatch:
-                return 1.01;
-            case BlockVerificationLevel.TypeMatch:
-                return 1.01;
-            case BlockVerificationLevel.Missing:
-                return 0.90;
-            default:
-                return 1.00;
         }
     }
 }
