cdb22913c949fb9c789cd6f0acc34c83d73f905b
ForestOfLight <briancassriel@gmail.com>
2026-07-29 19:29:09 -0700

    perf: skip target permutation lookup when performance rendering is active

--- packs/BP/scripts/classes/Render/VerificationRenderer.js
+++ packs/BP/scripts/classes/Render/VerificationRenderer.js
@@ -117,7 +117,7 @@ export class VerificationRenderer {
             dimension: dimension,
             location: this.instance.toGlobalCoords(location)
         };
-        const targetPermutation = this.instance.getBlockPermutation(location);
+        const targetPermutation = this.#usePerformanceRendering ? undefined : this.instance.getBlockPermutation(location);
         const blockVerificationLevelType = this.#usePerformanceRendering ? BlockVerificationLevelPerformanceRender : BlockPreviewVerificationLevelParticleRender;
         new blockVerificationLevelType(dimensionLocation, targetPermutation, verificationLevel, lifetime);
     }
